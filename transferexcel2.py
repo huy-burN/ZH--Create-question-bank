de3639adc0905eeba3170133bc6296d25d0648c2
huy-burN <nhathuy200712@gmail.com>
2025-04-22 16:22:03 +0700

    a2

--- transferexcel2.py
+++ transferexcel2.py
@@ -2,12 +2,12 @@ import json
 import pandas as pd
 
 # Load the JSON file
-with open('bankques.JSON', 'r', encoding='utf-8') as file:
+with open('bankq.JSON', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Extract data for each tab
-tab1_data = data.get("phần sức khỏe bệnh lý", [])
-tab2_data = data.get("phần thông tin sản phẩm", [])
+tab1_data = data.get("suc_khoe_benh_ly", [])
+tab2_data = data.get("thong_tin_san_pham", [])
 
 # Convert data to DataFrames
 tab1_df = pd.DataFrame(tab1_data, columns=["number", "type", "question", "answer", "difficulty"])
